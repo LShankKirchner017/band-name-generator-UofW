b4ac11db4ed04f2dae192dd3849dc0790a4bec66
Lindsay <lshank017@gmail.com>
2023-07-31 15:41:23 -0500

    inquirer

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 import { generateSlug } from 'random-word-slugs'
 import chalkAnimation from 'chalk-animation'
+import inquirer from 'inquirer'
 
-const numWords = parseInt(process.argv[2])
+console.log(chalkAnimation)
 
-const randomSlug = generateSlug(numWords, {
-    format: 'title'
-})
+inquirer
+  .prompt([
+    {
+      type: "number",
+      name: "numWords",
+      message: "How many words do you want in your band name?",
+      default: 3,
+    },
+    {
+      type: "list",
+      name: "animationName",
+      message: "Which animation do you want to use?",
+      choices: ['rainbow', 'pulse', 'glitch', 'radar', 'neon', 'karaoke'],
+      default: 'rainbow'
+    },
+  ])
+  .then((answers) => {
+    console.log(answers.animationName);
+  
+    const randomSlug = generateSlug(answers.numWords, {
+        format: 'title'
+    })
 
-chalkAnimation.rainbow(randomSlug)
+    chalkAnimation[answers.animationName](randomSlug)
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
 
